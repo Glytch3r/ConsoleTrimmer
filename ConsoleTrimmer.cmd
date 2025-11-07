5f78550fc518d3ae493da7de99f6acc6f135aae0
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2025-11-07 14:13:10 +0800

    update

--- ConsoleTrimmer.cmd
+++ ConsoleTrimmer.cmd
@@ -1,7 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Ask for the log file path
 set /p LOG_FILE=Enter full path of console.txt: 
 
 if not exist "%LOG_FILE%" (
@@ -10,21 +9,18 @@ if not exist "%LOG_FILE%" (
     exit /b
 )
 
-REM Ask for output file path
-set /p OUTPUT_FILE=Enter output file path (leave blank for same folder): 
+set /p OUTPUT_FILE=Enter output file path (leave blank for auto _trim.txt): 
 
 if "%OUTPUT_FILE%"=="" (
-    for %%I in ("%LOG_FILE%") do set "OUTPUT_FILE=%%~dpIconsole_trimmed.txt"
+    for %%I in ("%LOG_FILE%") do set "OUTPUT_FILE=%%~dpnI_trim.txt"
 )
 
 echo [INFO] Trimming logs...
 echo.
 
-REM Run PowerShell with proper escaping
-powershell -NoLogo -NoProfile -Command ^
-  "$content = Get-Content -Raw '%LOG_FILE%';" ^
-  "$pattern = '(?m)^(.*(LOG  : General|LOG  : Mo|LOG  : Network|DEBUG: General|WARN : Script|WARN : Recip|LOG  : Lua|AnimationAssetManager\.loadCallback|balObject\.require|AnimationPlayer\.play> Anim Clip not found|ImportedSkeleton\.collectBoneFrames> Could not find bone index for node name|DEBUG: Multiplayer|DEBUG: Voice|no GameSound|VehicleType\.initNormal> vehicle typ|no such function|Workshop: item state CheckItemState|ZomboidFileSystem\.loadModTileDefPropertyStrings|ZomboidFileSystem\.loadModTileDefs).*)$';" ^
-  "$content -split '\r?\n' | Where-Object {$_ -notmatch $pattern} | Set-Content '%OUTPUT_FILE%';"
+set "psCommand=$logFile = '%LOG_FILE%'; $outFile = '%OUTPUT_FILE%'; $content = Get-Content -Raw $logFile; $pattern = '(?m)^(.*(LOG  : General|IsoWorld.LoadTileDefinitions|not used by any GameSound|TextManager.Init> font|No such FMOD event|LOG  : Mo|LOG  : Network|DEBUG: General|WARN : Script|WARN : Recip|LOG  : Lua|AnimationAssetManager\.loadCallback|balObject\.require|AnimationPlayer\.play> Anim Clip not found|ImportedSkeleton\.collectBoneFrames> Could not find bone index for node name|DEBUG: Multiplayer|DEBUG: Voice|no GameSound|VehicleType\.initNormal> vehicle typ|no such function|Workshop: item state CheckItemState|ZomboidFileSystem\.loadModTileDefPropertyStrings|ZomboidFileSystem\.loadModTileDefs).*)$'; $content -split '\r?\n' | Where-Object {$_ -notmatch $pattern} | Set-Content -Path $outFile"
+
+powershell -NoLogo -NoProfile -Command "!psCommand!"
 
 if %errorlevel% neq 0 (
     echo [ERROR] Failed to trim logs.
